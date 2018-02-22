9be8c194dcf6a5eb715245de07a09d55ac2ad5da
keith smallpage <keith.smallpage@icloud.com>
2018-02-22 14:31:00 -0600

    initial commit

---
+++ LemonAidStand/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonAidStand
+{
+    class Player
+    {
+        // member variables
+
+        // constructor
+
+        // methods
+
+    }
+}
